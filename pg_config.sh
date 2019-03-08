d743ac9d31f54e6af617a66872606aa1e37e7081
Dani Santos <santos.adaniele@gmail.com>
2019-03-08 10:30:34 +0100

    Fixes issue with pip config

--- pg_config.sh
+++ pg_config.sh
@@ -3,7 +3,7 @@ DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -
 apt-get -qqy install postgresql python-psycopg2
 apt-get -qqy install python-sqlalchemy
 apt-get -qqy install python-pip
-pip install --upgrade pip
+pip install --upgrade pip==9.0.3
 pip install werkzeug==0.8.3
 pip install flask==0.9
 pip install Flask-Login==0.1.3
