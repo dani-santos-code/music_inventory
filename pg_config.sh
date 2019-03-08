3bc442d6dd97c87db68248d8d0cbdc0ba21040e2
Dani Santos <santos.adaniele@gmail.com>
2019-03-08 10:33:21 +0100

    fixes more issues on config file

--- pg_config.sh
+++ pg_config.sh
@@ -3,10 +3,11 @@ DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -
 apt-get -qqy install postgresql python-psycopg2
 apt-get -qqy install python-sqlalchemy
 apt-get -qqy install python-pip
-pip install --upgrade pip==9.0.3
+python -m pip
 pip install werkzeug==0.8.3
 pip install flask==0.9
-pip install Flask-Login==0.1.3
-pip install oauth2client
 pip install requests
 pip install httplib2
+pip install authlib==0.10
+pip install google-api-python-client
+pip install google-auth
