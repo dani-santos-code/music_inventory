28c00c3f25860ca8f11170092b3b160cbdc8db35
Dani Santos <santos.adaniele@gmail.com>
2019-03-09 18:57:28 +0100

    Implements authorization to delete and edit per user

--- catalog/main.py
+++ catalog/main.py
@@ -441,55 +441,73 @@ def add_new_instrument():
 # Update/Edit an instrument
 @app.route('/edit/<int:instrument_id>', methods=['GET', 'POST'])
 def edit_instrument(instrument_id):
-    if not is_logged_in():
-        return redirect('/login')
-    edited_instrument = session.\
-        query(Instrument).filter_by(id=instrument_id).one()
-    if request.method == 'POST':
-        if request.form['name']:
-            edited_instrument.name = request.form['name']
-        if request.form['region']:
-            edited_instrument.region = session.\
-                query(Region).filter_by(name=request.form['region']).one()
-        if request.form['description']:
-            edited_instrument.description = request.form['description']
-        if request.form['picture']:
-            edited_instrument.picture = request.form['picture']
-        if request.form['credit']:
-            edited_instrument.credit = request.form['credit']
-        session.add(edited_instrument)
-        session.commit()
-        flash('Instrument Successfully Edited')
-        return redirect(url_for('show_instrument_details',
-                                instrument_id=edited_instrument.id))
-    else:
-        user = get_user_info()
+    if is_logged_in():
+        current_user = get_user_info()
+        instrument_to_edit = session.\
+            query(Instrument).filter_by(id=instrument_id).one()
+        if float(current_user['id']) == float(instrument_to_edit.user_id):
+            if request.method == 'POST':
+                if request.form['name']:
+                    instrument_to_edit.name = request.form['name']
+                if request.form['region']:
+                    instrument_to_edit.region = session.\
+                        query(Region).filter_by(name=request.form['region']).one()
+                if request.form['description']:
+                    instrument_to_edit.description = request.form['description']
+                if request.form['picture']:
+                    instrument_to_edit.picture = request.form['picture']
+                if request.form['credit']:
+                    instrument_to_edit.credit = request.form['credit']
+                session.add(instrument_to_edit)
+                session.commit()
+                flash('Instrument Successfully Edited')
+                return redirect(url_for('show_instrument_details',
+                                    instrument_id=int(instrument_to_edit.id),
+                                    instrument=instrument_to_edit))
+        else:
+            flash('You are not authorized to edit this instrument!')
         return render_template('editinstrument.html',
-                               instrument_id=edited_instrument.id,
-                               instrument=edited_instrument,
-                               user=user,
-                               title="Edit Instrument")
-
+                                user=current_user,
+                                instrument=instrument_to_edit,
+                                title="Edit an Instrument")
+    elif not is_logged_in():
+        return redirect(url_for('show_login'))
+    else:
+        current_user = get_user_info()
+        return render_template('editinstrument.html',
+                                user=current_user,
+                                instrument=instrument_to_edit,
+                                title="Edit an Instrument")
 
 # Delete an instrument
 @app.route('/delete/<int:instrument_id>', methods=['GET', 'POST'])
 def delete_instrument(instrument_id):
     if not is_logged_in():
         return redirect('/login')
-    else:
-        user = get_user_info()
-    instrument_to_delete = session.\
-        query(Instrument).filter_by(id=instrument_id).one()
-    if request.method == 'POST':
-        session.delete(instrument_to_delete)
-        session.commit()
-        flash('Instrument Successfully Deleted')
-        return redirect(url_for('show_dashboard'))
-    else:
-        return render_template(
-            'deleteinstrument.html',
-            instrument_to_delete=instrument_to_delete,
-            user=user, title="Delete Instrument")
+    elif is_logged_in():
+        current_user = get_user_info()
+        instrument_to_delete = session.\
+            query(Instrument).filter_by(id=instrument_id).one()
+        if float(current_user['id']) == float(instrument_to_delete.user_id):
+            if request.method == 'POST':
+                print("POSTING")
+                session.delete(instrument_to_delete)
+                session.commit()
+                flash('Instrument Successfully Deleted')
+                return redirect(url_for('show_dashboard'))
+            else:
+                current_user = get_user_info()
+                return render_template('deleteinstrument.html',
+                                       instrument_to_delete=instrument_to_delete,
+                                       user=current_user,
+                                       title="Delete Instrument")
+        else:
+            flash("You don't have permission to delete this item!")
+            return render_template('deleteinstrument.html',
+                                   instrument_to_delete=instrument_to_delete,
+                                   user=current_user,
+                                   title="Delete Instrument")
+
 
 
 if __name__ == '__main__':
